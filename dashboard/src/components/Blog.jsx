ae3bf9f2d5cb8be8bb65c803960c774b8cdb113f
Zubair Ibn Zamir <zizdjbd@gmail.com>
2026-05-04 09:44:38 +0600

    Fix blog table rendering with remark-gfm and add table styles

--- dashboard/src/components/Blog.jsx
+++ dashboard/src/components/Blog.jsx
@@ -1,10 +1,11 @@
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import blogContent from '../blog.md?raw'
 
 export default function Blog() {
   return (
     <div className="blog-prose">
-      <ReactMarkdown>{blogContent}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{blogContent}</ReactMarkdown>
     </div>
   )
 }
